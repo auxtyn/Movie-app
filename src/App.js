3c276c90d66d9dccbd22cf9ed8e4705241694a8d
Auxtyn <austinserver042@gmail.com>
2023-11-18 18:39:59 +0100

    Checking

--- src/App.js
+++ src/App.js
@@ -1,63 +1,46 @@
 
 import React from 'react'
-import Movie from './components/Movie'
+import MovieCard from './components/MovieCard'
 import AddMovie from './components/AddMovie'
+import axios from 'axios'
+import Nav from './components/Nav'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+const API_URL = 'https://api.themoviedb.org/3/movie/popular?api_key=32434d8aedd8cdecaf3b72ebaca02f48'
 
 const App = () => {
-  const [movies, setMovies] = useState(
-    [
-      {
-        id: 1,
-        title: "Avengers",
-        description: "Action",
-        posterURL: "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/108b520c55e3c9760f77a06110d6a73b_e97cf224-d57f-44e3-8477-4f5479cd746b_480x.progressive.jpg?v=1573616089",
-        rating: 5
-      },
-      {
-        id: 2,
-        title: "Loki",
-        description: "Action",
-        posterURL: "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/54362_2_480x.progressive.png.jpg?v=1634831916",
-        rating: 4
-      },
-      {
-        id: 3,
-        title: "Wanda Vision",
-        description: "Bloody",
-        posterURL: "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/wandavision.mp_480x.progressive.jpg?v=1614371756",
-        rating: 5
-      },
-      {
-        id: 4,
-        title: "Thor",
-        description: "Action",
-        posterURL: "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/d8994c1a1ce870c6da978c950ffb5278_62899afe-82e0-4c72-a60b-85d7e912daee_500x749.jpg?v=1573587378",
-        rating: 5
-      }
-    ]
-  )
+  const [movies, setMovies] = useState([])
+
+  const findMovies = () => {
+    axios(API_URL)
+      .then((response) => {
+        const res = response.data
+        setMovies(res.results)
+      })
+  }
+
+
+  useEffect(() => {
+    findMovies()
+  }, [])
 
   const deleteMovie = (id) => {
     setMovies(movies.filter((movie) => movie.id !== id))
   }
 
   const filterRating = () => {
-    setMovies(movies.filter((movie) => movie.rating >= 5))
+    setMovies(movies.filter((movie) => movie.vote_average >= 8))
   }
 
-  const filterDescription = () => {
-    setMovies(movies.filter((movie) => movie.description === 'Bloody'))
-  }
+  // const filterDescription = () => {
+  //   setMovies(movies.filter((movie) => movie.description === 'Bloody'))
+  // }
 
-  const filterAction = () => {
-    setMovies(movies.filter((movie) => movie.description === 'Action'))
-  }
-
+  // const filterAction = () => {
+  //   setMovies(movies.filter((movie) => movie.description === 'Action'))
+  // }
   // Add movie
-
-
   const addMovie = (movie) => {
     const id = Math.floor(Math.random() * 10000) + 1
     const newMov = { id, ...movie }
@@ -67,19 +50,21 @@ const App = () => {
   return (
     <div>
       <div className="container text-center">
+        <Nav />
         <h1 className='text-center'>Movies</h1>
-        
+
         <AddMovie onAdd={addMovie} />
         <br></br>
 
-        <button onClick={filterRating} className='btn text-center btn-info'>5 star movies</button>
-        <button onClick={filterDescription} className='btn text-center btn-info'>Bloody movies</button>
-        <button onClick={filterAction} className='btn text-center btn-info'>Action movies</button>
+        <button onClick={filterRating} className='btn text-center btn-info'>Highest rated movies</button>
+        <br></br>
 
+        {movies.length > 0 ?
+          movies.map((movie) => (
 
-
-
-        {movies.length > 0 ? <Movie movies={movies} deleteMovie={deleteMovie} addMovie={addMovie}></Movie> : <p className='text-info'>No movies to view</p>}
+            <MovieCard className="col-md-4" movie={movie} deleteMovie={deleteMovie} key={movie.id} />
+          ))
+          : <p className='text-info'>No movies to view</p>}
       </div>
     </div>
   )
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,17 @@
 
+
+
 const MovieCard = ({ movie, deleteMovie }) => {
+  const API_IMG = 'https://image.tmdb.org/t/p/original/';
+
   return (
-    <div className="movie-card-outer">
+    <div className="movie-card-outer col-md-3">
       <div className="movie-card ">
         <div className="card">
-          <img src={movie.posterURL} alt={movie.title} />
-          <h5>{movie.description}</h5>
-          <h4>{movie.title}</h4>
-          <p className='text-warning'>Rating:{movie.rating} stars</p>
+          <img src={API_IMG + movie.poster_path} alt={movie.title} />
+          <p>{movie.release_date}</p>
+          <h6>{movie.title}</h6>
+          <p className='text-warning'>Rating:{movie.vote_average}</p>
           <button onClick={() => deleteMovie(movie.id)} className='btn text-center btn-damger'>Delete movie</button>
         </div>
       </div>
