c8af3e696f3e6c008e61c8f43e89aefaea2ca512
Auxtyn <austinserver042@gmail.com>
2023-11-20 21:05:22 +0100

    Commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,10 @@
 const About = () => {
     return (
         <>
-            <div className='mt-5 pt-5 about'>
+            <div className='mt-5 pt-5 about pb-4'>
                 <h1 className="text-white pt-5">Who we are</h1>
-                <h5 className="pb-5 pt-3 px-5">FilmSpot is an internet TV service. What sets FilmSpot apart is a unique combination of hit African content, first and exclusive international series, movies, the best kids’ shows, and live sport. FilmSpot was born in 2015. The service is available throughout sub-Saharan Africa and to selected diaspora markets worldwide.
-                </h5>
+                <p className="pb-5 pt-3 px-5">FilmSpot is an internet TV service. What sets FilmSpot apart is a unique combination of hit African content, first and exclusive international series, movies, the best kids’ shows, and live sport. FilmSpot was born in 2015. The service is available throughout sub-Saharan Africa and to selected diaspora markets worldwide.
+                </p>
             </div>
         </>
     )
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,23 +3,23 @@ import React from 'react'
 const Carousel = () => {
   return (
     <>
-      <div id="carouselExampleFade" className="carousel slide carousel-fade container">
+      <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
         <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src="images/pexels1.jpg" className="d-block w-100" alt="Cinema" />
+          <div className="carousel-item active" data-bs-interval="1000">
+            <img src="images/pexels1.jpg" className="d-block w-100" alt="..." />
           </div>
-          <div className="carousel-item">
-            <img src="images/pexels3.jpg" className="d-block w-100" alt="Cinema" />
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src="images/pexels3.jpg" className="d-block w-100" alt="..." />
           </div>
-          <div className="carousel-item">
-            <img src="images/pexels2.jpg" className="d-block w-100" alt="Cinema" />
+          <div className="carousel-item" data-bs-interval="1000">
+            <img src="images/pexels2.jpg" className="d-block w-100" alt="..." />
           </div>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Previous</span>
         </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
           <span className="carousel-control-next-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Next</span>
         </button>
