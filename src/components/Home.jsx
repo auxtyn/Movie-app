2e20da7b1c2e915da1fcef7a9ae9b235d8690040
auxtyn <51249310+auxtyn@users.noreply.github.com>
2023-11-24 10:58:19 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ import Hero from './Hero'
 import Single from './Single'
 import { Routes, Route } from 'react-router-dom'
 
-const API_URL = 'https://api.themoviedb.org/3/movi/popular?api_key=32434d8aedd8cdecaf3b72ebaca02f48'
+const API_URL = 'https://api.themov32434d8aedd8cdecaf3b72ebaca02f48'
 
 const Home = () => {
 
