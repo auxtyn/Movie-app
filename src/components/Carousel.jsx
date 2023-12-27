034ca815acb8b5fee521264669b2e82d4d1e990c
Auxtyn <austinserver042@gmail.com>
2023-12-27 13:38:25 +0100

    commit

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
 import { selectMovies } from "../features/movieSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 const Carousel = () => {
   let movie = useSelector(selectMovies);
